b8b33fe511f5101cab273769432da2d313ff040a
Lawrence Tan <lawrence.tan@mongodb.com>
2018-10-17 14:01:02 -0400

    first function

---
+++ stitch_app/functions/getMatch/source.js
@@ -0,0 +1,19 @@
+exports = async function(reqBody){
+  const playerId = reqBody.playerId;
+  const mongodb = context.services.get("mongodb-atlas");
+  const matchRequests = mongodb.db("core").collection("matchRequests");
+
+  // Read data from a collection
+  const matchRequest = await matchRequests.findOne({ playerId: playerId });
+  if (matchRequest === undefined) {
+    return {status:"failed"}
+  }
+  if ('matchId' in matchRequest) {
+    if (matchRequest.matchId === null) {
+      return {status:"waiting"}
+    } else {
+      return {status:"ok", matchId: matchRequest.matchId}
+    }
+  }
+  return {status:"failed"}
+};
